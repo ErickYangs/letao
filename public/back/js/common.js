e5d0ecc68bb1bdc27c7dc27e30912543a0d68c40
ErickYangs <1114341853@qq.com>
2018-08-07 17:36:36 +0800

    公共功能完成,实现登录拦截完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -2,7 +2,29 @@
  * 实现进度条功能
  */
 
-// NProgress.start();
+// 5. 判断用户是否登录,实现登录拦截
+/**
+ *  (1) 用户已登录,继续访问
+ *  (2) 用户未登录,拦截到登录页
+ */
+// 一进入页面,发送ajax请求
+if (location.href.indexOf('login.html') === -1) {
+  // 不是login页面,进行登录页面
+  $.ajax({
+    type: 'get',
+    url: '/employee/checkRootLogin',
+    dataType: 'json',
+    success: function (info) {
+      if (info.success) {
+        console.log('已登录');
+      }
+      if (info.error === 400) {
+        location.href = 'login.html';
+      }
+    }
+  });
+}
+
 
 
 // 在发送第一个ajax请求时,开启进度条,
@@ -19,5 +41,39 @@ $(document).ajaxStop(function () {
   // 模拟网络延迟
   setTimeout(function () {
     NProgress.done();
-  },500);
+  }, 500);
+});
+
+
+
+$(function () {
+  // 公共功能实现
+  // 1. 左侧二级菜单切换
+  $('.lt-aside .category').click(function () {
+    $('.lt-aside .child').stop().slideToggle();
+  });
+  // 2. 点击切换侧边栏
+  $('.icon-menu').click(function () {
+    $('.lt-aside').toggleClass('hidemenu');
+    $('.lt-main').toggleClass('hidemenu');
+    $('.lt-topbar').toggleClass('hidemenu');
+  });
+  // 3. 点击退出菜单,显示退出模态框
+  $('.icon-logout').click(function () {
+    $('#logoutModal').modal('show');
+  });
+  // 4.点击退出按钮,实现用户退出
+  $('#logoutBth').click(function () {
+    // 需发送ajax请求
+    $.ajax({
+      type: 'get',
+      url: '/employee/employeeLogout',
+      dataType: 'json',
+      success: function (info) {
+        if (info.success) {
+          location.href = 'login.html';
+        }
+      }
+    });
+  });
 });
