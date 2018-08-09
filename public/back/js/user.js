acbb9953fa8e4bc9baff59cb67c3472241274939
ErickYangs <1114341853@qq.com>
2018-08-09 11:43:41 +0800

    用户管理完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -2,6 +2,8 @@ $(function () {
 
   var currentPage = 1;
   var pageSize = 5;
+  var currentId; // 当前选中的用户id
+  var isDelete;
   // 1- 渲染数据
   render();
   function render() {
@@ -35,6 +37,38 @@ $(function () {
     });
   }
 
+  // 3- 点击启用禁用按钮,显示模态框,通过事件委托
+  $('tbody').on('click','.btn',function () {
+    // 显示模态框
+    $('#userModal').modal('show');
 
+    // 获取用户 id
+    currentId = $(this).parent().data('id');
+
+    isDelete = $(this).hasClass('btn-danger') ? 0 : 1;
+    
+  });
+
+  // 4- 点击确认按钮,发送ajax请求,修改对应用户状态
+  $('#submitBtn').click(function () {
+    $.ajax({
+      url: '/user/updateUser',
+      type: 'post',
+      data: {
+        id: currentId,
+        isDelete: isDelete
+      },
+      dataType:'json',
+      success: function (info) {
+        if (info.success) {
+          //1- 关闭模态框
+          $('#userModal').modal('hide');
+          //2- 页面重新渲染
+          render();
+        }
+
+      }
+    });
+  });
 
 });
